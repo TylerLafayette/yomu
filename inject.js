15776867b109e9bce16375a77c2d07a5bdc149cb
Tyler Lafayette <tylerlafayette@gmail.com>
2019-11-17 21:29:02 -0800

    Fix highlighting issues with position

--- inject.js
+++ inject.js
@@ -3,17 +3,21 @@ const _XHROpen = window.XMLHttpRequest.prototype.open
 // Quick and dirty fix for a weird issue with kuromoji.js where it treats
 // the URL like a path.
 window.XMLHttpRequest.prototype.open = function(a, b, c) {
-    if (b.includes('https:/kuromojin')) {
+    if (b.includes("https:/kuromojin")) {
         arguments[1] = arguments[1].replace(
-            'https:/kuromojin',
-            'https://kuromojin'
+            "https:/kuromojin",
+            "https://kuromojin"
         )
     }
     return _XHROpen.apply(this, arguments)
 }
 
 class Yomu {
-    searchTags = ['p']
+    searchTags = [
+        ...new Array(6).fill().map((_, i) => `h${i + 1}`),
+        "span",
+        "p",
+    ]
     constructor() {
         this.tokenizer = null
     }
@@ -21,7 +25,7 @@ class Yomu {
     init = _ => {
         kuromoji
             .builder({
-                dicPath: 'https://kuromojin.netlify.com/dict/',
+                dicPath: "https://kuromojin.netlify.com/dict/",
             })
             .build((err, tokenizer) => {
                 this.tokenizer = tokenizer
@@ -36,17 +40,35 @@ class Yomu {
     // processTag processes a single tag.
     processTag = tag => {
         document.querySelectorAll(tag).forEach(item => {
-            const tokenized = this.tokenizer.tokenize(item.innerHTML)
+            let tokenized = this.tokenizer.tokenize(item.innerHTML)
             let content = item.innerHTML
+
+            // Split the content into an array of every character to allow for injection later.
+            let split = content.split("")
+
+            // Filter out the bad stuff.
+            tokenized = tokenized.filter(
+                token =>
+                    !token.surface_form.match(
+                        /[0-9]|[a-z]|[A-Z]|[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|[ ]|[「」『』、。：？；]/
+                    )
+            )
+
+            // For each token, add the yomu-highlighted-word element around the word.
             tokenized.forEach(token => {
-                if (token.word_type !== 'KNOWN') return
-                content = content.replace(
-                    new RegExp(token.surface_form, 'g'),
-                    `<a class="yomu-highlighted-word" href="#">${token.surface_form}</a>`
-                )
+                split[
+                    token.word_position - 1
+                ] = `<yomu-highlighted-word class="yomu-highlighted-word">${
+                    split[token.word_position - 1]
+                }`
+
+                split[token.word_position - 2 + token.surface_form.length] = `${
+                    split[token.word_position - 2 + token.surface_form.length]
+                }</yomu-highlighted-word>`
             })
 
-            item.innerHTML = content
+            // Join the array again and replace the inner content of the tag with it.
+            item.innerHTML = split.join("")
         })
     }
 }
