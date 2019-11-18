15ee4e681b0e8f8c28ca4fdc6e870388195a019b
Tyler Lafayette <tylerlafayette@gmail.com>
2019-11-17 23:33:20 -0800

    Added popups with style

--- inject.js
+++ inject.js
@@ -20,9 +20,10 @@ class Yomu {
     ]
     constructor() {
         this.tokenizer = null
+        this.mount = document.createElement("yomu-mount")
     }
     // init loads the dictionary and builds the tokenizer.
-    init = _ => {
+    init = async _ => {
         kuromoji
             .builder({
                 dicPath: "https://kuromojin.netlify.com/dict/",
@@ -32,6 +33,64 @@ class Yomu {
                 window.tokenizer = tokenizer
                 this.process()
             })
+
+        document.body.prepend(this.mount)
+    }
+    // attachListeners attaches listeners to the highlighted words in the DOM.
+    attachListeners = _ => {
+        document
+            .querySelectorAll("yomu-highlighted-word")
+            .forEach(item =>
+                item.addEventListener("click", this.highlightClick)
+            )
+    }
+    // highlightClick opens a prompt when a highlighted word is clicked.
+    highlightClick = async e => {
+        this.mount.innerHTML = ""
+        e.target.classList.add("thinking")
+
+        const word = e.target.innerHTML
+
+        const request = await fetch(
+            `https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words?keyword=${word}`,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    Origin: location.origin,
+                    "X-Requested-With": "XMLHttpRequest",
+                },
+            }
+        )
+
+        const json = await request.json()
+
+        if (!json.data) return e.target.classList.remove("thinking")
+
+        if (!json.data[0]) return e.target.classList.remove("thinking")
+
+        const vocab = json.data[0]
+        const japanese = vocab.japanese[0]
+        const senses = vocab.senses[0]
+
+        const element = document.createElement("yomu-dictionary-box")
+
+        element.innerHTML += `<span class="yomu-dictionary-reading">${
+            japanese.reading
+        }</span><span class="yomu-dictionary-title">${
+            japanese.word
+        }</span><p class="definitions">${senses.english_definitions.join(
+            ", "
+        )}</p>`
+
+        const bound = e.target.getBoundingClientRect()
+
+        element.style.bottom = window.innerHeight - bound.y + "px"
+        element.style.left = bound.x + "px"
+
+        this.mount.append(element)
+        e.target.classList.remove("thinking")
     }
     // process looks through the current tab and attempts to process text.
     process = async _ => {
@@ -50,7 +109,7 @@ class Yomu {
             tokenized = tokenized.filter(
                 token =>
                     !token.surface_form.match(
-                        /[0-9]|[a-z]|[A-Z]|[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|[ ]|[「」『』、。：？；]/
+                        /[0-9]|[a-z]|[A-Z]|[@#-•!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|[ ]|[「」『』、。：？；]/
                     )
             )
 
@@ -70,6 +129,9 @@ class Yomu {
             // Join the array again and replace the inner content of the tag with it.
             item.innerHTML = split.join("")
         })
+
+        // Attach new listeners to highlighted words.
+        this.attachListeners()
     }
 }
 
