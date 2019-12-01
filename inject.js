b14a2417af766ba7c0105289baa6842f75db5712
Tyler Lafayette <tylerlafayette@gmail.com>
2019-12-01 15:51:50 -0800

    [Resolves #1] Created a web worker that handles tokenization and dictionary lookups asychronously

--- inject.js
+++ inject.js
@@ -6,6 +6,13 @@
 // dictionary entries from this API URL.
 const API_URL = "https://yomu-api.herokuapp.com"
 
+// Action constants.
+const TOKENIZE = "TOKENIZE"
+const TOKENIZE_COMPLETED = "TOKENIZE_COMPLETED"
+const DICTIONARY_LOOKUP = "DICTIONARY_LOOKUP"
+const DICTIONARY_LOOKUP_COMPLETED = "DICTIONARY_LOOKUP_COMPLETED"
+const DICTIONARY_LOOKUP_ERROR = "DICTIONARY_LOOKUP_ERROR"
+
 const _XHROpen = window.XMLHttpRequest.prototype.open
 
 // Quick and dirty fix for a weird issue with kuromoji.js where it treats
@@ -31,31 +38,46 @@ class Yomu {
         ...new Array(6).fill().map((_, i) => `h${i + 1}`),
         "span",
         "p",
+        "div*",
     ]
     blockedTags = ["html", "body", "script", "object"]
-    tokenizeQueue = []
+    tokenizeQueue = {}
+    dictionaryQueue = {}
     constructor() {
-        this.tokenizer = null
+        // The yomu-mount is where all dictionary-boxes are appended to
+        // when displayed.
         this.mount = document.createElement("yomu-mount")
     }
     // init loads the dictionary and builds the tokenizer.
     init = async _ => {
-        kuromoji
-            .builder({
-                dicPath: `${API_URL}/public/vendor/dict/`,
-            })
-            .build((err, tokenizer) => {
-                this.tokenizer = tokenizer
-                window.tokenizer = tokenizer
-                this.process()
-            })
+        this.process()
 
+        // Add the yomu-mount to the end of the body.
         document.body.prepend(this.mount)
-        this.sendMessage({ greeting: "je" })
     }
     // messagePipe receives and interprets messages from the yomu worker.
     messagePipe = (request, sender, sendResponse) => {
-        alert(`Received ${request.farewell}`)
+        // Get the action from the request.
+        // In this model, the request functions similarly to a Redux
+        // action and the action attribute signals what type of action to
+        // dispatch.
+        const { action } = request
+
+        console.log(action)
+
+        switch (action) {
+            case TOKENIZE_COMPLETED:
+                this.applyElement(request.elementId, request.tokenized)
+                break
+            case DICTIONARY_LOOKUP_COMPLETED:
+                this.createDictionaryPopup(request.requestId, request.json)
+                break
+            case DICTIONARY_LOOKUP_ERROR:
+                this.dictionaryError(request.requestId)
+                break
+            default:
+                break
+        }
     }
     // sendMessage sends a message to the yomu worker.
     sendMessage = message => {
@@ -63,6 +85,8 @@ class Yomu {
     }
     // attachListeners attaches listeners to the highlighted words in the DOM.
     attachListeners = _ => {
+        // Find all yomu-highlighted-words and attach them to the
+        // highlightClick function.
         document
             .querySelectorAll("yomu-highlighted-word")
             .forEach(item =>
@@ -100,39 +124,58 @@ class Yomu {
         // Clear it anyway.
         this.clearMount()
 
+        // Add the thinking class to the element to indicate to the client
+        // that the definition is loading.
         e.target.classList.add("thinking")
+        // Remove the error class in case it was previously set.
         e.target.classList.remove("error")
 
-        const request = await fetch(`${API_URL}/dictionary?keyword=${word}`, {
-            method: "GET",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                Origin: location.origin,
-                "X-Requested-With": "XMLHttpRequest",
-            },
+        // Generate a pseudo-random ID for the request using the element's
+        // height plus a random number.
+        const requestId =
+            e.target.clientHeight + Math.floor(Math.random() * 1000000000000)
+
+        // Add an object to the dictionary queue for this item.
+        this.dictionaryQueue[requestId] = {
+            item: e.target,
+            word,
+        }
+
+        // Send the request to the worker.
+        this.sendMessage({
+            action: DICTIONARY_LOOKUP,
+            requestId,
+            word,
         })
+    }
+    // createDictionaryPopup appends a dictionary-box to the mount based on
+    // a response received from the worker.
+    createDictionaryPopup = (requestId, json) => {
+        // Get the item and word requested from the dictionary queue.
+        const { item, word } = this.dictionaryQueue[requestId]
 
-        const json = await request.json()
-
-        if (!json.data) return this.failed(e.target)
-
-        if (!json.data[0]) return this.failed(e.target)
+        // Throw an error to the client if anything went wrong.
+        if (!json.data) return this.failed(item)
+        if (!json.data[0]) return this.failed(item)
 
         const vocab = json.data[0]
         const japanese = vocab.japanese[0]
         const senses = vocab.senses[0]
 
+        // Create a new yomu-dictionary-box element to display the
+        // definition to the user.
         const element = document.createElement("yomu-dictionary-box")
 
-        element.innerHTML += `<span class="yomu-dictionary-reading">${
-            japanese.reading
-        }</span><span class="yomu-dictionary-title">${japanese.word ||
+        // Add the information to the innerHTML of the dictionary-box.
+        element.innerHTML += `<span class="yomu-dictionary-reading">${japanese.reading ||
+            japanese.word}</span><span class="yomu-dictionary-title">${japanese.word ||
             japanese.reading}</span><p class="definitions">${senses.english_definitions.join(
             ", "
         )}</p>`
 
-        const bound = e.target.getBoundingClientRect()
+        // Get the position of the clicked text in order to make
+        // calculations on where to show the dictionary-box.
+        const bound = item.getBoundingClientRect()
 
         element.style.bottom =
             window.innerHeight -
@@ -140,9 +183,25 @@ class Yomu {
             "px"
         element.style.left = bound.x + "px"
 
+        // Append the element and add it to the openWords array.
         this.mount.append(element)
         this.openWords.push(word)
-        e.target.classList.remove("thinking")
+
+        // Reset the loading state.
+        item.classList.remove("thinking")
+
+        // Clear the queue item.
+        delete this.dictionaryQueue[requestId]
+    }
+    // dictionaryError shows an error on a failed dictionary lookup.
+    dictionaryError = ({ requestId }) => {
+        // Get the item from the dictionary queue.
+        const { item } = this.dictionaryQueue[requestId]
+        // Show the failed indicator.
+        this.failed(item)
+
+        // Clear the queue item.
+        delete this.dictionaryQueue[requestId]
     }
     // process looks through the current tab and attempts to process text.
     process = async _ => {
@@ -155,17 +214,15 @@ class Yomu {
         document
             .querySelectorAll(tag)
             .forEach(item => this.processElement(item, dangerous))
-
-        // Attach new listeners to highlighted words.
-        this.attachListeners()
     }
+    // processElement processes a single element for tokenization.
     processElement = async (item, dangerous) => {
-        if (item.children.length > 0)
+        if (Array.from(item.children).filter(x => x.tagName != "BR").length > 0)
             if (dangerous) return null
             else
-                Array.from(item.children).forEach(item =>
-                    this.processElement(item, dangerous)
-                )
+                Array.from(item.children)
+                    .filter(x => x.tagName != "BR")
+                    .forEach(item => this.processElement(item, dangerous))
 
         if (this.blockedTags.includes(item.tagName.toLowerCase())) return
 
@@ -180,21 +237,31 @@ class Yomu {
         const elementId =
             item.clientHeight + Math.floor(Math.random() * 1000000000000)
 
+        // Add an object to the token queue for this item.
+        this.tokenizeQueue[elementId] = {
+            item,
+            content,
+        }
+
+        // Send the TOKENIZE request to the worker.
         this.sendMessage({
-            action: "TOKENIZE",
+            action: TOKENIZE,
             elementId,
             content: content.replace(/[<](.){1,}(>)(<\/)(.){1,}(>)/g, match =>
                 "*".repeat(match.length)
             ),
         })
+    }
+    // applyElement applies highlights to an element after its content has
+    // been tokenized.
+    applyElement = async (elementId, tokenized) => {
+        // Get the item from the tokenize queue.
+        if (!this.tokenizeQueue[elementId]) return
+        let { item, content } = this.tokenizeQueue[elementId]
 
-        let tokenized = this.tokenizer.tokenize(
-            content.replace(/[<](.){1,}(>)(<\/)(.){1,}(>)/g, match =>
-                "*".repeat(match.length)
-            )
-        )
+        if (!item || !content) return
 
-        // Filter out the bad stuff.
+        // Filter out the bad stuff. (Non-Japanese characters, etc.)
         tokenized = tokenized.filter(
             token =>
                 !token.surface_form.match(
@@ -220,9 +287,18 @@ class Yomu {
 
         // Join the array again and replace the inner content of the tag with it.
         if (split.join("") != item.innerHTML) item.innerHTML = split.join("")
+
+        // Re-attach new listeners.
+        this.attachListeners()
+
+        // Delete the item from the queue.
+        delete this.tokenizeQueue[elementId]
     }
 }
 
+// Initialize the Yomu class.
 const yomu = new Yomu()
+// Attach the Chrome message listener to the Yomu message pipe.
 chrome.runtime.onMessage.addListener(yomu.messagePipe)
+// Initialize Yomu.
 yomu.init()
--- worker.js
+++ worker.js
@@ -6,6 +6,13 @@
 // dictionary entries from this API URL.
 const API_URL = "https://yomu-api.herokuapp.com"
 
+// Action constants.
+const TOKENIZE = "TOKENIZE"
+const TOKENIZE_COMPLETED = "TOKENIZE_COMPLETED"
+const DICTIONARY_LOOKUP = "DICTIONARY_LOOKUP"
+const DICTIONARY_LOOKUP_COMPLETED = "DICTIONARY_LOOKUP_COMPLETED"
+const DICTIONARY_LOOKUP_ERROR = "DICTIONARY_LOOKUP_ERROR"
+
 class YomuWorker {
     tokenizer = null
     constructor() {}
@@ -19,18 +26,64 @@ class YomuWorker {
                 this.tokenizer = tokenizer
             })
     }
-    // messagePipe receives and interprets messages from the content script.
+    // messagePipe receives and interprets messages from the inject script.
     messagePipe = (request, sender, sendResponse) => {
-        alert(`Received ${request.greeting}`)
-        this.sendMessage({ farewell: "hhhh" })
+        const { action } = request
+
+        switch (action) {
+            case TOKENIZE:
+                this.tokenizeMessage(request)
+                break
+            case DICTIONARY_LOOKUP:
+                this.dictionaryLookup(request)
+                break
+            default:
+                break
+        }
+    }
+    // tokenizeMessage takes a message from the inject script, tokenizes
+    // it, then replies with the tokens.
+    tokenizeMessage = ({ elementId, content }) => {
+        // Tokenize the received content.
+        const tokenized = this.tokenizer.tokenize(content)
+        // Send a message back to the inject script with the tokens.
+        this.sendMessage({
+            action: TOKENIZE_COMPLETED,
+            elementId,
+            tokenized,
+        })
+    }
+    // dictionaryLookup looks up a word on the dictionary, then replies to
+    // the inject script with it.
+    dictionaryLookup = async ({ requestId, word }) => {
+        try {
+            const request = await fetch(`${API_URL}/dictionary?keyword=${word}`)
+            const json = await request.json()
+            this.sendMessage({
+                action: DICTIONARY_LOOKUP_COMPLETED,
+                requestId,
+                json,
+            })
+        } catch (e) {
+            this.sendMessage({
+                action: DICTIONARY_LOOKUP_ERROR,
+                requestId,
+            })
+        }
     }
     // sendMessage sends a message to the active tab.
     sendMessage = message => {
+        // Get the active Chrome tab.
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            // Send a message to the found tab.
             chrome.tabs.sendMessage(tabs[0].id, message)
         })
     }
 }
 
+// Initialize the YomuWorker.
 const worker = new YomuWorker()
+// Attach the Chrome message listener to the YomuWorker's messagePipe.
 chrome.runtime.onMessage.addListener(worker.messagePipe)
+// Initialize the YomuWorker.
+worker.init()
